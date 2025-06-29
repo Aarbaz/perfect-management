873969ae23513500fc4c82d149036de16ead7d51
Arbaz Ansari <abzansari19@gmail.com>
2025-06-30 00:55:59 +0530

    s issue

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 // Create axios instance with base configuration
 const api = axios.create({
   // baseURL: process.env.REACT_APP_API_URL || 'http://localhost:8080',
-  baseURL: 'https://terrific-celebration-production-30b0.up.railway.app/',// for production
+  baseURL: 'http://terrific-celebration-production-30b0.up.railway.app/',// for production
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
